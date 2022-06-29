ca88b50740ce24a4cdc5378c260ea4cfe2c9aba0
IYED-LH <iyedhmidi@gmail.com>
2022-06-29 02:31:39 +0100

    search bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>hello</h1>
+  <main>
+
+ <SearchBox/>
+  
+  </main>
 </template>
 
 <script>
 
+import SearchBox from './components/SearchBox.vue' 
+
 export default {
   name: 'App',
+  components: {
+    SearchBox
+  },
+  
+  
   data() {
     return {
       api_key: 'cced478c6c43ec001db0af42549b8e28'
@@ -17,4 +28,66 @@ export default {
 </script>
 
 <style>
+@import url('http://fonts.cdnfonts.com/css/montserrat');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('./assets/summerbg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+#app.warm {
+  background-image: url('./assets/winterbg.jpg');
+}
+main {
+  min-height: 100vh;
+  padding: 30px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.50));
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
 </style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,44 @@
+<template>
+  
+<div >
+  <input class="search-bar" type="text" placeholder="search...">  
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'SearchBox',
+  
+  
+}
+</script>
+
+<style scoped>
+
+.search-bar {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 75%;
+  padding: 15px;
+  margin-bottom: 30px;
+  
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-bar:focus {
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 25px 25px 25px 25px;
+}
+
+</style>
